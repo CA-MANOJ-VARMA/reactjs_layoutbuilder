a3a18030d9b50d37b72dae3a28c774939d77a59e
CA-MANOJ-VARMA <camanojvarma@gmail.com>
2023-03-05 12:42:05 +0000

    MAR 05

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -27,41 +27,37 @@ const ConfigurationController = () => (
       return (
         <div className="css-configurationcontroller-container">
           <h1>Layout</h1>
-          <ul className="css-ul-checkbox-container">
-            <li>
-              <div className="css-checkbox-container">
-                <input
-                  type="checkbox"
-                  id="content"
-                  onChange={contentFunction}
-                  checked={showContent && `checked`}
-                />
-                <label htmlFor="content">Content</label>
-              </div>
-            </li>
-            <li>
-              <div className="css-checkbox-container">
-                <input
-                  type="checkbox"
-                  id="Left Navbar"
-                  onChange={leftnavabarFunction}
-                  checked={showLeftNavbar && `checked`}
-                />
-                <label htmlFor="Left Navbar">Left Navbar</label>
-              </div>
-            </li>
-            <li>
-              <div className="css-checkbox-container">
-                <input
-                  type="checkbox"
-                  id="Right Navbar"
-                  onChange={rightnavabarFunction}
-                  checked={showRightNavbar && `checked`}
-                />
-                <label htmlFor="Right Navbar">Right Navbar</label>
-              </div>
-            </li>
-          </ul>
+          <div className="css-ul-checkbox-container">
+            <div className="css-checkbox-container">
+              <input
+                type="checkbox"
+                id="content"
+                onChange={contentFunction}
+                checked={showContent && `checked`}
+              />
+              <label htmlFor="content">Content</label>
+            </div>
+
+            <div className="css-checkbox-container">
+              <input
+                type="checkbox"
+                id="Left Navbar"
+                onChange={leftnavabarFunction}
+                checked={showLeftNavbar && `checked`}
+              />
+              <label htmlFor="Left Navbar">Left Navbar</label>
+            </div>
+
+            <div className="css-checkbox-container">
+              <input
+                type="checkbox"
+                id="Right Navbar"
+                onChange={rightnavabarFunction}
+                checked={showRightNavbar && `checked`}
+              />
+              <label htmlFor="Right Navbar">Right Navbar</label>
+            </div>
+          </div>
         </div>
       )
     }}
